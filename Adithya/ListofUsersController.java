ce50988240c1e56be29a00e964d0159af2ed7a6a
asing508 <148830630+asing508@users.noreply.github.com>
2024-10-08 20:22:52 -0700

    Add files via upload

---
+++ Adithya/ListofUsersController.java
@@ -0,0 +1,15 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+public class ListofUsersController {
+
+    @FXML
+    private Button homeButton;
+
+    @FXML
+    private ListView<?> myListView;
+
+}
---
+++ Adithya/ResetUserAccountPageController.java
@@ -0,0 +1,19 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ResetUserAccountPageController {
+
+    @FXML
+    private Button homeButton;
+    
+
+    @FXML
+    private Button resetButton;
+
+    @FXML
+    private TextField resetTxt;
+
+}
